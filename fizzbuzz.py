e0faa2d026cc224f60d47b2b212fe387a61a43cc
2384star <2384star@naver.com>
2023-05-01 19:42:21 +0900

    update : fizzbuzz2.0

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for j in range(1,16+1):
-    if j%15==0:
-        print('fizzbuzz')
-    elif j%3==0:
-        print('fizz')
-    elif j%5==0:
-        print('buzz')
-    else:
-        print(f'{j}')
